be77cc9f9457186d8f612a742ff3282234b880d5
tryccatch <tryccatch@gmail.com>
2023-10-09 22:39:54 +0800

    21.实现背包检查和添加物品
    InventoryManager
    CheckCapacity
    AddItemAtIndex

--- tutorials/Farm/Assets/Scripts/Inventory/Logic/InventoryManager.cs
+++ tutorials/Farm/Assets/Scripts/Inventory/Logic/InventoryManager.cs
@@ -26,20 +26,73 @@ namespace TA.Inventory
         /// <param name="toDestroy">是否要销毁物品</param>
         public void AddItem(Item item, bool toDestroy)
         {
-            // 背包是否有空位
             // 是否已经有该物品
-            InventoryItem newItem = new()
-            {
-                itemID = item.itemID,
-                itemAmount = 1
-            };
+            var index = GetItemIndexInBag(item.itemID);
 
-            playerBag.itemList[0] = newItem;
+            AddItemAtIndex(item.itemID, index, 1);
 
             Debug.Log(item.itemDetails.itemID + "\tName:" + item.itemDetails.itemName);
             if (toDestroy)
             {
                 Destroy(item.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// 检查背包是否有空位
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckBagCapacity()
+        {
+            for (int i = 0; i < playerBag.itemList.Count; i++)
+            {
+                if (playerBag.itemList[i].itemID == 0)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 通过物品ID找到背包已有物品位置
+        /// </summary>
+        /// <param name="ID">物品ID</param>
+        /// <returns>-1则没有这个物品否则返回序号</returns>
+        private int GetItemIndexInBag(int ID)
+        {
+            for (int i = 0; i < playerBag.itemList.Count; i++)
+            {
+                if (playerBag.itemList[i].itemID == ID)
+                    return i;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// 在指定背包序号位置添加物品
+        /// </summary>
+        /// <param name="ID">物品ID</param>
+        /// <param name="index">序号</param>
+        /// <param name="amount">数量</param>
+        private void AddItemAtIndex(int ID, int index, int amount)
+        {
+            if (index == -1 && CheckBagCapacity())    // 背包没有这个物品 同时背包有空位
+            {
+                var item = new InventoryItem { itemID = ID, itemAmount = amount };
+                for (int i = 0; i < playerBag.itemList.Count; i++)
+                {
+                    if (playerBag.itemList[i].itemID == 0)
+                    {
+                        playerBag.itemList[i] = item;
+                        break;
+                    }
+                }
+            }
+            else    // 背包有这个物品
+            {
+                int currentAmount = playerBag.itemList[index].itemAmount + amount;
+                var item = new InventoryItem { itemID = ID, itemAmount = currentAmount };
+
+                playerBag.itemList[index] = item;
             }
         }
     }
