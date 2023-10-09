4483644e36d4c08a50cbd766c3eca97a89e4a45c
tryccatch <tryccatch@gmail.com>
2023-10-09 14:02:08 +0800

    16.（Editor）实现 ListView 添加删除同步信息功能
    如果遇到查找 ItemDataList_SO 丢失引用的情况，请留意视频里提到不同版本 t: 的问题。另外也留意 if 判断 dataArray >= 1 详细请看以下详细代码。
    
    //其他所有变量的绑定

--- tutorials/Farm/Assets/Editor/UI Builder/ItemEditor.cs
+++ tutorials/Farm/Assets/Editor/UI Builder/ItemEditor.cs
@@ -60,12 +60,39 @@ public class ItemEditor : EditorWindow
         itemListView = root.Q<VisualElement>("ItemList").Q<ListView>("ListView");
         itemDetailsSection = root.Q<ScrollView>("ItemDetails");
         iconPreview = itemDetailsSection.Q<VisualElement>("Icon");
+
+        // 获取按键
+        root.Q<Button>("AddButton").clicked += OnAddItemClicked;
+        root.Q<Button>("DeleteButton").clicked += OnDeleteClicked;
+
         // 加载数据
         LoadDataBase();
 
         // 生成ListView
         GenerateListView();
     }
+
+    #region 按键事件
+    private void OnAddItemClicked()
+    {
+        ItemDetails newItem = new()
+        {
+            itemName = "NEW ITEM",
+            itemID = 1000 + itemList.Count
+        };
+        itemList.Add(newItem);
+
+        itemListView.Rebuild();
+    }
+
+    private void OnDeleteClicked()
+    {
+        itemList.Remove(activeItem);
+
+        itemListView.Rebuild();
+        itemDetailsSection.visible = false;
+    }
+    #endregion
 
     private void LoadDataBase()
     {
@@ -119,6 +146,7 @@ public class ItemEditor : EditorWindow
     {
         itemDetailsSection.MarkDirtyRepaint();
 
+        // General
         itemDetailsSection.Q<IntegerField>("ItemID").value = activeItem.itemID;
         itemDetailsSection.Q<IntegerField>("ItemID").RegisterValueChangedCallback(evt =>
         {
@@ -132,6 +160,13 @@ public class ItemEditor : EditorWindow
             itemListView.Rebuild();
         });
 
+        itemDetailsSection.Q<EnumField>("ItemType").Init(activeItem.itemType);
+        itemDetailsSection.Q<EnumField>("ItemType").value = activeItem.itemType;
+        itemDetailsSection.Q<EnumField>("ItemType").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.itemType = (ItemType)evt.newValue;
+        });
+
         iconPreview.style.backgroundImage = activeItem.itemIcon == null ? defaultIcon.texture : activeItem.itemIcon.texture;
         itemDetailsSection.Q<ObjectField>("ItemIcon").value = activeItem.itemIcon;
         itemDetailsSection.Q<ObjectField>("ItemIcon").RegisterValueChangedCallback(evt =>
@@ -141,6 +176,57 @@ public class ItemEditor : EditorWindow
 
             iconPreview.style.backgroundImage = newIcon == null ? defaultIcon.texture : newIcon.texture;
             itemListView.Rebuild();
+        });
+
+        itemDetailsSection.Q<ObjectField>("ItemSprite").value = activeItem.itemOnWorldSprite;
+        itemDetailsSection.Q<ObjectField>("ItemSprite").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.itemOnWorldSprite = (Sprite)evt.newValue;
+        });
+
+        // Description
+        itemDetailsSection.Q<TextField>("Description").value = activeItem.itemDescription;
+        itemDetailsSection.Q<TextField>("Description").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.itemDescription = evt.newValue;
+        });
+
+        // Properties
+        itemDetailsSection.Q<IntegerField>("ItemUseRadius").value = activeItem.itemUseRadius;
+        itemDetailsSection.Q<IntegerField>("ItemUseRadius").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.itemUseRadius = evt.newValue;
+        });
+
+        itemDetailsSection.Q<Toggle>("CanPickedup").value = activeItem.canPickedup;
+        itemDetailsSection.Q<Toggle>("CanPickedup").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.canPickedup = evt.newValue;
+        });
+
+        itemDetailsSection.Q<Toggle>("CanDropped").value = activeItem.canDropped;
+        itemDetailsSection.Q<Toggle>("CanDropped").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.canDropped = evt.newValue;
+        });
+
+        itemDetailsSection.Q<Toggle>("CanCarried").value = activeItem.canCarried;
+        itemDetailsSection.Q<Toggle>("CanCarried").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.canCarried = evt.newValue;
+        });
+
+        // vendor
+        itemDetailsSection.Q<IntegerField>("Price").value = activeItem.itemPrice;
+        itemDetailsSection.Q<IntegerField>("Price").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.itemPrice = evt.newValue;
+        });
+
+        itemDetailsSection.Q<Slider>("SellPercentage").value = activeItem.sellPercentage;
+        itemDetailsSection.Q<Slider>("SellPercentage").RegisterValueChangedCallback(evt =>
+        {
+            activeItem.sellPercentage = evt.newValue;
         });
     }
 }
