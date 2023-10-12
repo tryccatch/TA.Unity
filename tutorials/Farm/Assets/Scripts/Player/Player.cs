c2386f21e1652595424fde61a31e8c00278b7ee1
tryccatch <tryccatch@gmail.com>
2023-10-12 21:19:22 +0800

    32.制作 Player 的动画
    由于我已经将动画都为大家制作好了，目前拿到的素材版本人物的 BodyWalk 和 BodyRun 是有 Event 播放音效的。使用的时候没有代码会报错，可以手动在 Animation 中删掉 Event。稍后我会更新素材解决这个问题。
    Animator Controller
    Idle Walk Run animations
    Player
    SiwtchAnimation

--- tutorials/Farm/Assets/Scripts/Player/Player.cs
+++ tutorials/Farm/Assets/Scripts/Player/Player.cs
@@ -10,14 +10,19 @@ public class Player : MonoBehaviour
 
     private Vector2 movementInput;
 
+    private Animator[] animators;
+    private bool isMoving;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animators = GetComponentsInChildren<Animator>();
     }
 
     private void Update()
     {
         PlayerInput();
+        SwitchAnimation();
     }
 
     private void FixedUpdate()
@@ -38,11 +43,33 @@ public class Player : MonoBehaviour
             inputY *= 0.6f;
         }
 
+        // 走路状态速度
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            inputX *= 0.5f;
+            inputY *= 0.5f;
+        }
+
         movementInput = new Vector2(inputX, inputY);
+
+        isMoving = movementInput != Vector2.zero;
     }
 
     private void Movement()
     {
         rb.MovePosition(rb.position + speed * Time.deltaTime * movementInput);
+    }
+
+    private void SwitchAnimation()
+    {
+        foreach (var anim in animators)
+        {
+            anim.SetBool("isMoving", isMoving);
+            if (isMoving)
+            {
+                anim.SetFloat("InputX", inputX);
+                anim.SetFloat("InputY", inputY);
+            }
+        }
     }
 }
