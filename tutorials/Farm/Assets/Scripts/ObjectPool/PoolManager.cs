bc08fe7128f3ffbe43b8be14a9301a05c3810883
tryccatch <tryccatch@gmail.com>
2023-10-18 23:03:54 +0800

    66.（Crop）制作石头和稻草的粒子特效

--- tutorials/Farm/Assets/Scripts/ObjectPool/PoolManager.cs
+++ tutorials/Farm/Assets/Scripts/ObjectPool/PoolManager.cs
@@ -49,6 +49,8 @@ public class PoolManager : MonoBehaviour
         {
             ParticleEffectType.LeavesFalling01 => poolEffectList[0],
             ParticleEffectType.LeavesFalling02 => poolEffectList[1],
+            ParticleEffectType.Rock => poolEffectList[2],
+            ParticleEffectType.ReapableScenery => poolEffectList[3],
             _ => null,
         };
 
