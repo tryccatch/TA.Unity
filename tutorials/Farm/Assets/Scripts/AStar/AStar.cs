6b2a13e839d22e23054856b604e8ea6291751df6
tryccatch <tryccatch@gmail.com>
2023-10-27 23:15:09 +0800

    70.（AStar）核心功能评估周围节点得到最短路径

--- tutorials/Farm/Assets/Scripts/AStar/AStar.cs
+++ tutorials/Farm/Assets/Scripts/AStar/AStar.cs
@@ -35,7 +35,6 @@ namespace TA.AStar
             }
         }
 
-
         /// <summary>
         /// 构建网格节点信息，初始化两个列表
         /// </summary>
@@ -85,7 +84,7 @@ namespace TA.AStar
             return true;
         }
 
-        public bool FindShortestPath()
+        private bool FindShortestPath()
         {
             // 添加起点
             openNodeList.Add(startNode);
@@ -106,12 +105,79 @@ namespace TA.AStar
                 }
 
                 // 计算周围8个Node补充到OpenList
-
+                EvaluateNeighbourNodes(closeNode);
             }
 
             return pathFound;
         }
 
+        /// <summary>
+        /// 评估周围8个点，并生成对应消耗值
+        /// </summary>
+        /// <param name="currentNode"></param>
+        private void EvaluateNeighbourNodes(Node currentNode)
+        {
+            Vector2Int currentNodePos = currentNode.gridPosition;
+            Node validNeighbourNode;
 
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    validNeighbourNode = GetValidNeighbourNode(currentNodePos.x + x, currentNodePos.y + y);
+
+                    if (validNeighbourNode != null)
+                    {
+                        if (!openNodeList.Contains(validNeighbourNode))
+                        {
+                            validNeighbourNode.gCost = currentNode.gCost + GetDistance(currentNode, validNeighbourNode);
+                            validNeighbourNode.hCost = GetDistance(validNeighbourNode, targetNode);
+                            // 链接父节点
+                            validNeighbourNode.parentNode = currentNode;
+                            openNodeList.Add(validNeighbourNode);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 找到有效的Node,非障碍，非已选择
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        private Node GetValidNeighbourNode(int x, int y)
+        {
+            if (x >= gridWidth || y >= gridHeight || x < 0 || y < 0)
+                return null;
+
+            Node neighbourNode = gridNodes.GetGridNode(x, y);
+
+            if (neighbourNode.isObstacle || closedNodeList.Contains(neighbourNode))
+                return null;
+            else
+                return neighbourNode;
+        }
+
+        /// <summary>
+        /// 返回两点距离值
+        /// </summary>
+        /// <param name="nodeA"></param>
+        /// <param name="nodeB"></param>
+        /// <returns>14的倍数+10的倍数</returns>
+        private int GetDistance(Node nodeA, Node nodeB)
+        {
+            int xDistance = Mathf.Abs(nodeA.gridPosition.x - nodeB.gridPosition.x);
+            int yDistance = Mathf.Abs(nodeA.gridPosition.y - nodeB.gridPosition.y);
+
+            if (xDistance > yDistance)
+                return 14 * yDistance + 10 * (xDistance - yDistance);
+
+            return 14 * xDistance + 10 * (yDistance - xDistance);
+        }
     }
 }
