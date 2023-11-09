1434fa9e1a06c742b8b7fe67b9c1e1035b2c434d
tryccatch <tryccatch@gmail.com>
2023-11-09 17:35:50 +0800

    5_2.（Audio）创建 AudioMixer 实现音乐音效的控制和切换

--- tutorials/Farm/Assets/Scripts/Audio/Logic/AudioManager.cs
+++ tutorials/Farm/Assets/Scripts/Audio/Logic/AudioManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
@@ -12,6 +13,16 @@ public class AudioManager : MonoBehaviour
     public AudioSource gameSource;
 
     private Coroutine soundRoutine;
+
+    [Header("Audio Mixer")]
+    public AudioMixer audioMixer;
+
+    [Header("Snapshots")]
+    public AudioMixerSnapshot normalSnapShot;
+    public AudioMixerSnapshot ambientSnapShot;
+    public AudioMixerSnapshot muteSnapShot;
+    private float musicTransitionSecond = 8f;
+
     public float MusicStartSecond => Random.Range(5f, 15f);
 
     private void OnEnable()
@@ -44,9 +55,9 @@ public class AudioManager : MonoBehaviour
     {
         if (music != null && ambient != null)
         {
-            PlayAmbientClip(ambient);
+            PlayAmbientClip(ambient, 1f);
             yield return new WaitForSeconds(MusicStartSecond);
-            PlayMusicClip(ambient);
+            PlayMusicClip(music, musicTransitionSecond);
         }
     }
 
@@ -54,21 +65,32 @@ public class AudioManager : MonoBehaviour
     /// 播放环境音效
     /// </summary>
     /// <param name="soundDetails"></param>
-    private void PlayAmbientClip(SoundDetails soundDetails)
+    private void PlayAmbientClip(SoundDetails soundDetails, float transitionTime)
     {
+        audioMixer.SetFloat("AmbientVolume", ConvertSoundVolume(soundDetails.soundVolume));
         ambientSource.clip = soundDetails.soundClip;
         if (ambientSource.isActiveAndEnabled)
             ambientSource.Play();
+
+        ambientSnapShot.TransitionTo(transitionTime);
     }
 
     /// <summary>
     /// 播放背景音乐
     /// </summary>
     /// <param name="soundDetails"></param>
-    private void PlayMusicClip(SoundDetails soundDetails)
+    private void PlayMusicClip(SoundDetails soundDetails, float transitionTime)
     {
+        audioMixer.SetFloat("MusicVolume", ConvertSoundVolume(soundDetails.soundVolume));
         gameSource.clip = soundDetails.soundClip;
         if (gameSource.isActiveAndEnabled)
             gameSource.Play();
+
+        normalSnapShot.TransitionTo(transitionTime);
+    }
+
+    private float ConvertSoundVolume(float amount)
+    {
+        return amount * 100 - 80;
     }
 }
