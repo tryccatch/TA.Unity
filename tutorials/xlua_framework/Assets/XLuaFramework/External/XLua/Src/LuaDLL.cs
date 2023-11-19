257c5b43edcd7a6e4625dc496c8d3e0155cc121d
tryccatch <tryccatch@gmail.com>
2023-11-19 18:14:52 +0800

    XLua_037：编译XLua源码来集成第三方库Protobuf和RapidJson

--- tutorials/xlua_framework/Assets/XLuaFramework/External/XLua/Src/LuaDLL.cs
+++ tutorials/xlua_framework/Assets/XLuaFramework/External/XLua/Src/LuaDLL.cs
@@ -39,6 +39,35 @@ namespace XLua.LuaDLL
         const string LUADLL = "xlua";
 #endif
 
+        //Lua使用RapidJson
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_rapidjson(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadRapidJson(System.IntPtr L)
+        {
+            return luaopen_rapidjson(L);
+        }
+
+        //LUA支持PROTOBUF
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_protobuf_c(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadProtobufC(System.IntPtr L)
+        {
+            return luaopen_protobuf_c(L);
+        }
+
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_pb(System.IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadPb(System.IntPtr L)
+        {
+            return luaopen_pb(L);
+        }
+
         [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr lua_tothread(IntPtr L, int index);
 
