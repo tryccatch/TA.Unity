565744a7339e160b414fc40a886623ba9740fa98
tryccatch <tryccatch@gmail.com>
2023-11-19 14:07:25 +0800

    XLua_014：实现打AB包工具类ABEditor(三)(生成AB配置文件)

--- tutorials/xlua_framework/Assets/XLuaFramework/Scripts/AssetLoader.cs
+++ tutorials/xlua_framework/Assets/XLuaFramework/Scripts/AssetLoader.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 /// <summary>
 /// 模块资源加载器
--- tutorials/xlua_framework/Assets/XLuaFramework/Scripts/Downloader.cs
+++ tutorials/xlua_framework/Assets/XLuaFramework/Scripts/Downloader.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 /// <summary>
 /// 下载器 工具类
